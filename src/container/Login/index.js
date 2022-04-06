03bc8eb349ae55951394f3a9ea1acc6b4126412e
Jackwang2017 <m18701822457@163.com>
2022-04-06 20:23:56 +0800

    updates

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Input, Form, Checkbox, Button } from 'antd';
+import { Form } from '@ant-design/compatible';
+import '@ant-design/compatible/assets/index.css';
+import { Input, Checkbox, Button } from 'antd';
 
 class Login extends Component {
   constructor() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './components/App'
-import Login from './container/Login'
+import Login from './container/Login/index'
 // import './styles/style.sass'
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
